e9150857f4e836acd73d261d283de467f2dfc5cd
Kevin Chow <kevin.c@reconinstruments.com>
2015-03-18 09:47:37 -0700

    whoops, a second commit
    Change-Id: I3d3b8d54f6176efd63d4e75a120350e9fb050b94

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 
 int main(void)
 {
-  Rectangle my_rect();
+  Rectangle my_rect(3, 4);
+  std::cout << my_rect.getVertices() << std::endl;
   for (int i = 10; i > 0; i--)
 	{
 	  std::cout << i << std::endl;
